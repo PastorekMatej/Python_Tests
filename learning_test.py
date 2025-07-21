8463589c44d9997a17247e678ed4a45cef6dcc78
PastorekMatej <matejpastorek22@gmail.com>
2025-07-21 08:58:04 +0200

    test: update of the learning_test.py file to integrate the tests of the merge dictionnaries

--- learning_test.py
+++ learning_test.py
@@ -1,10 +1,16 @@
 import pytest 
 from format_conversion import conversion
+from merge_dictionnaries import merge_dictionnaries
 
 def test_conversion():
     assert conversion("345") == 345
     assert conversion("3.456") == 3
 
-
+def test_merge_dictionnaries():
+    dict1 = {"user_id" : "Matej", "conversation" : {"t1":"123"}, "life" : "is beautiful" }
+    dict2 = {"user_id" : "Jozed", "conversation" : {"t1":"123"}, "what" : "is true"}
+    assert merge_dictionnaries(dict1,dict2) ==  {"user_id" : "Jozed", "conversation": {"t1":"123"}, "life" : "is beautiful", "what" :"is true"}
+    assert merge_dictionnaries(dict1,dict2) ==  {"user_id" : "Jozed", "conversation": {"t1":"123"}, "what" : "is true", "life" : "is beautiful"}
+    assert merge_dictionnaries(dict1,dict2) ==  {"user_id" : "Matej", "conversation": {"t1":"123"}, "what" : "is true", "life" : "is beautiful"}
 
 
