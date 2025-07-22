6d0815a712f6624c45e711986917855cec55c9f9
PastorekMatej <matejpastorek22@gmail.com>
2025-07-22 11:01:47 +0200

    feat:add commit_elements.py

---
+++ common_elements.py
@@ -0,0 +1,24 @@
+#rewrite the code using Pythonic approach and write pytest unity test
+
+def common_elements(list1:list,list2:list) ->list:
+    """ Find common elements in list
+
+        Function finds common elements in 2 lists 
+    Args:
+    list1 (list): 1. list
+    list2 (list): 2. list
+
+    Return:
+    list
+"""
+    j=[]
+    for i in list1:
+            if i in list2:
+                    j.append(i)
+        
+    return j
+
+
+if __name__ == "__main__":
+        print(common_elements(["1",2,3,"4"],["1","4"]))
+              
